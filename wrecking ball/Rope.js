1bdb42caab8b510d75f3e23c961bfd9e2dccf826
vishwapandya <57280185+vishwapandya@users.noreply.github.com>
2021-04-23 11:32:37 +0530

    Update Rope.js

--- wrecking ball/Rope.js
+++ wrecking ball/Rope.js
@@ -8,26 +8,14 @@ class Rope {
 
 
   }
-  attach(body) {
-    this.rope.bodyA = body;
-  }
 
-  fly() {
-    this.rope.bodyA = null;
-  }
 
   display() {
-    if (this.rope.bodyA) {
-      var pointA = this.rope.bodyA.position;
-      var pointB = this.pointB;
-      push();
-
-      stroke(48, 22, 8);
-      strokeWeight(3);
-
-      line(pointB.x, pointB.y, pointA.x, pointA.y);
-
-      pop();
-    }
+    
+    
+    
+    
+    
+    
   }
 }
